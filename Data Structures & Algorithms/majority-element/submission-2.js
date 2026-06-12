c4578a8ffab0dedf4f8a1da5acd7496b42e9ce00
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-06-12 13:04:19 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        // brute force
+        // let n = nums.length
+        // let floor = Math.floor(n/2)
+        // for(let num of nums){
+        //     let count = nums.reduce((acc,curr)=>acc + (curr == num ? 1 : 0),0)
+        //     if(count > floor){
+        //         return num
+        //     }
+        // }
+        // return -1
+
+        // optimized
+        let map = new Map()
+        let res = 0
+        let count = 0
+        for(let num of nums){
+            map.set(num,(map.get(num) || 0)+1)
+            if(map.get(num) > count){
+                res = num
+                count = map.get(num)
+            }
+        }
+        return res
+    }
+}
