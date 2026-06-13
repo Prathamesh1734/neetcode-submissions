70cb75cc09d6c0e722f7e6c9b3ee4a56caabd9a6
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-06-13 12:05:41 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        let res = []
+        for(let num of nums1){
+            let nextGreater = -1
+            for(let i=nums2.length-1;i>=0;i--){
+                if(nums2[i] > num){
+                    nextGreater = nums2[i]
+                }else if(nums2[i] == num){
+                    break
+                }
+            }
+            res.push(nextGreater)
+        }
+        return res
+    }
+}
