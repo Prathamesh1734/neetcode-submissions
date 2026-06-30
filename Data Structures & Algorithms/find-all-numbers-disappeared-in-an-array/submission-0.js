4d0501a1fe4d7b33ee73c8fa3cd021ee55f3a0d4
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-06-30 17:49:05 +0530

    Add: find-all-numbers-disappeared-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    findDisappearedNumbers(nums) {
+        let set = new Set()
+        for(let i=1;i<=nums.length;i++){
+            set.add(i)
+        }
+
+        for(let num of nums){
+            set.delete(num)
+        }
+
+        return Array.from(set)
+    }
+}
