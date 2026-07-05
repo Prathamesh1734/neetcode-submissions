da6b7d9d83093c4baf4147f66a5feb7e5141fc6e
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-07-05 15:44:57 +0530

    Add: find-lucky-integer-in-an-array - submission-1

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number}
+     */
+    findLucky(arr) {
+        let map = new Map()
+        for(let e of arr){
+            map.set(e,(map.get(e) || 0) + 1)
+        }
+        let res = -1
+        for(let [num,count] of map.entries()){
+            if(num === count){
+                res = Math.max(res,num)
+            }
+        }
+        return res
+    }
+}
