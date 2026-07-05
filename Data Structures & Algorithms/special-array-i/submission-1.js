3d6adf85b1c0f0535b9e624b171d615d3cd3f0b7
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-07-05 16:33:00 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isArraySpecial(nums) {
+        if(nums.length == 1) return true
+        for(let i=1;i<nums.length;i++){
+            if(nums[i-1] % 2 === nums[i] % 2) return false
+        }
+        return true
+    }
+}
