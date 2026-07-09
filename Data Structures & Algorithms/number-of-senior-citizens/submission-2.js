ff4565b933e7695d9f95c8575bc46e1a8164aa8b
Pratham <92156272+Prathamesh1734@users.noreply.github.com>
2026-07-09 16:57:20 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string[]} details
+     * @return {number}
+     */
+    countSeniors(details) {
+        let count = 0
+        for(let detail of details){
+            let age = Number(detail.slice(11,13))
+            if(age > 60) count++
+        }
+        return count
+    }
+}
